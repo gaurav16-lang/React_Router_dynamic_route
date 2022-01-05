1081a14e5a5ede622292d4bb2fe3e340e0e66d39
Gaurav Maihuria <maihuriagaurav16@gmail.com>
2022-01-05 21:20:20 +0530

    completed

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,6 +1,28 @@
 import { useParams } from 'react-router-dom'
+import React from 'react'
 
 export const UsersDetails = () => {
-  const { userid } = useParams()
-  return <div>Fetch data of user:{userid}</div>
+  const [user, setuser] = React.useState({})
+  const [isLoading, setIsloading] = React.useState(false)
+  const { usersid } = useParams()
+  const getuserdetail = () => {
+    fetch(`https://reqres.in/api/users/${usersid}`)
+      .then((d) => d.json())
+      .then((res) => {
+        console.log(res.data)
+        setuser(res.data)
+      })
+  }
+  React.useEffect(() => {
+    getuserdetail()
+  }, [])
+
+  return (
+    <div>
+      <img src={user.avatar} alt="h" />
+      <p>{user.email}</p>
+      <p>{user.first_name}</p>
+      <p>{user.last_name}</p>
+    </div>
+  )
 }
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,18 +1,59 @@
 import { Link } from 'react-router-dom'
+import React from 'react'
+import styled from 'styled-components'
 
+const Wraper = styled.div`
+  width: 50%;
+
+  border: 1px solid red;
+  display: grid;
+  grid-template-columns: repeat(4, 20%);
+  grid-gap: 1%;
+  margin: auto;
+  margin-top: 50px;
+`
+const Image = styled.img`
+  max-width: 100%;
+  display: inline-block;
+`
+const Belowraper = styled.div`
+  width: 100%;
+  border: 1px solid black;
+`
 export const Users = () => {
+  const [users, setusers] = React.useState([])
+  const [isLoading, setisLoading] = React.useState(true)
+
+  const detail = (e) => {
+    console.log(e)
+  }
+
+  const fetchUser = () => {
+    setisLoading(true)
+    fetch(`https://reqres.in/api/users`)
+      .then((d) => d.json())
+      .then((res) => {
+        setusers(res.data)
+        console.log(res.data)
+      })
+  }
+  React.useEffect(() => {
+    fetchUser()
+  }, [])
+
   return (
-    <div>
-      {[
-        { name: 'SmartPhone', price: 30, id: 0 },
-        { name: 'Watch', price: 20, id: 1 },
-        { name: 'Shirt', price: 50, id: 2 },
-        { name: 'Pants', price: 100, id: 3 },
-      ].map((e, i) => {
-        ;<div key={i} style={{ margin: '10px' }}>
-          <Link to={`/users/${e}`}>Users{e}</Link>
-        </div>
-      })}
-    </div>
+    <Wraper>
+      {users.map((e, i) => (
+        <Link to={`/users/${e.id}`} key={i}>
+          <Belowraper>
+            <Image src={e.avatar} />
+          </Belowraper>
+        </Link>
+      ))}
+    </Wraper>
   )
+}
+// style={{ margin: '10px' }}
+{
+  /*  */
 }
